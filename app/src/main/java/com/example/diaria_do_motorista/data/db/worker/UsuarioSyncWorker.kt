8af77d2498c8213e6db9afa2ef23950610d01189
joanamarta <joanamartasouza@gmail.com>
2026-01-30 21:08:57 -0300

    30/01/2025 PROJETO EM ANDAMENTO

--- app/src/main/java/com/example/diaria_do_motorista/data/db/worker/UsuarioSyncWorker.kt
+++ app/src/main/java/com/example/diaria_do_motorista/data/db/worker/UsuarioSyncWorker.kt
@@ -3,6 +3,8 @@ package com.example.diaria_do_motorista.data.db.worker
 import android.content.Context
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkerParameters
 import com.example.diaria_do_motorista.data.db.repository.DiariaRepository
 import com.example.diaria_do_motorista.data.db.repository.TransportadoraRepository
@@ -21,11 +23,28 @@ class UsuarioSyncWorker @AssistedInject constructor(
     private val networkMonitor: NetworkMonitor
 
 ):CoroutineWorker(context,params){
-    return try{
+  companion object{
+      private const val TAG ="UsuarioSyncWorker"
+      const val WORK_NAME = "usuario_sync_work"
 
-    }
+      //COnstates para configuração do Worker
+      const val KEY_FORCE_SYNC = "force_sync"
+      const val KEY_SYNC_TYPE = "sync-type"
 
-    override suspend fun doWork(): Result {
-        TODO("Not yet implemented")
-    }
+
+      // Tipos de sincronização
+      const val SYNC_ALL = "all"
+      const val SYNC_USERS_ONLY = "users_only"
+      const val SYNC_PENDING = "pending_only"
+
+      fun creteWorkRequest(
+          forceSync:Boolean = false,
+          syncType:String = SYNC_ALL,
+          delayMinutes:Long = 0
+      ): OneTimeWorkRequest {
+          val inputData = Data.Builder()
+              .putBoolean(KEY_FORCE_SYNC, forceSync)
+              .putString(KEY_SYNC_TYPE, syncType)
+              .build()
+  }
 }
---
+++ app/src/main/java/com/example/diaria_do_motorista/sync/exeptions/Exception.kt
@@ -0,0 +1,4 @@
+package com.example.diaria_do_motorista.sync.exeptions
+
+class SyncException (message: String): Exception (message) {
+}
