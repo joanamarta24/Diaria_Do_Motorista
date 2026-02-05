ff2e5445952eeb102145191e0f756ac2e0d9357d
joanamarta <joanamartasouza@gmail.com>
2026-02-04 21:26:16 -0300

    04/02 ALGUNS METODOS ADICIONADOS

--- app/src/main/java/com/example/diaria_do_motorista/data/db/domain/Diaria.kt
+++ app/src/main/java/com/example/diaria_do_motorista/data/db/domain/Diaria.kt
@@ -1,7 +1,23 @@
 package com.example.diaria_do_motorista.data.db.domain
 
+import com.example.diaria_do_motorista.data.db.remote.enums.DiariaStatus
+
 data class Diaria(
     val id: String,
     val motoristaId: String,
-    val
+    val matriculaVeiculo: String,
+    val transportadoraId: String,
+    val dataDiaria: String,
+    val destino: String,
+    val horaInicio: String,
+    val horaFim: String? = null,
+    val kmInicio: Double,
+    val kmFim: Double? = null,
+    val totalPortagens: Double? = null,
+    val abastecimento: Boolean = false,
+    val abastecimentoAdBlue: Boolean = false,
+    val observacoes: String? = null,
+    val status: DiariaStatus = DiariaStatus.EM_ANDAMENTO,
+    val horasTrabalhadas: Double? = null,
+    val kmRodados: Double? = null
 )
