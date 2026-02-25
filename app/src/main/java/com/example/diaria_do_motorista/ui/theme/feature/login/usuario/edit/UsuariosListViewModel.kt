5a3ff355dee246f469ff1e1aefb4b3d91a089bbb
joana <joanamartasouza@gmail.com>
2026-02-24 21:37:30 -0300

    24/02/2026 ALGUNS CODIGO ADICIONADO E CORRIGIDO ALGUMAS ALTERACAO NO CODIGO UsuariosListScreen.kt TENTADO CORRIGIR O CODIGO ATUAL

--- app/src/main/java/com/example/diaria_do_motorista/ui/theme/feature/login/usuario/edit/UsuariosListViewModel.kt
+++ app/src/main/java/com/example/diaria_do_motorista/ui/theme/feature/login/usuario/edit/UsuariosListViewModel.kt
@@ -1,5 +1,34 @@
 package com.example.diaria_do_motorista.ui.theme.feature.login.usuario.edit
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.diaria_do_motorista.data.db.repository.UsuarioRepository
+import com.example.diaria_do_motorista.ui.theme.feature.login.usuario.list.UsuariosListViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class UsuariosListViewModel  @Inject constructor(
+    private val usuarioRepository: UsuarioRepository,
+    private val savedStateHandle: SavedStateHandle
+): ViewModel() {
+    private  val _uiState = MutableStateFlow(UsuarioEditUiState())
+    val uiState: StateFlow<UsuarioEditUiState> = _uiState.asStateFlow()
+
+    private val usuarioId: String? = savedStateHandle["UsuarioId"]
+
+    init {
+        if (usuarioId != null && usuarioId != "new") {
+            _uiState.update { it.copy(isEditMode = true) }
+            loadUsuario(usuarioId)
+        }
+        loadTransportadoras()
+    }
 
 
-fun UsuariosListViewModel (){}
+
+}
